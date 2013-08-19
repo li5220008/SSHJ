607e8936de0d4c3b34f81872158c1e3a498d88f3
Free <li5220008@163.com>
2013-08-19 18:44:56 +0800

    Signed-off-by: Free <li5220008@163.com>

---
+++ src/cn/itcast/bean/Person.java
@@ -0,0 +1,57 @@
+package cn.itcast.bean;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Person implements Serializable{
+	private Integer id;
+	private String name;
+	public Person(){}
+	public Person(String name) {
+		super();
+		this.name = name;
+	}
+	@Id @GeneratedValue
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	@Column(length=10, nullable=false)
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Person other = (Person) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+}
